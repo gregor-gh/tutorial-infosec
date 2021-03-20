2a55ca3ca7c87baa8c8e570d76d60d2e701f76be
gregor-gh <gregor@live.co.uk>
2021-03-20 19:46:44 +0000

    Added hsts

--- myApp.js
+++ myApp.js
@@ -9,7 +9,10 @@ app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-
+app.use(helmet.hsts({
+  maxAge: 7776000,
+  force: true
+}));
 
 
 
